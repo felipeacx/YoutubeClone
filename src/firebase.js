06ef9770b8820e750919c0a2515472edf5ca50ba
Felipe Acosta <juanfe1190@hotmail.com>
2020-08-18 21:13:26 -0500

    Initial commit.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADF7ABIkdzCn-LfHizfjNSMg440EHnBZY",
+    authDomain: "clone-4323c.firebaseapp.com",
+    databaseURL: "https://clone-4323c.firebaseio.com",
+    projectId: "clone-4323c",
+    storageBucket: "clone-4323c.appspot.com",
+    messagingSenderId: "569815839228",
+    appId: "1:569815839228:web:1a3ca96ff67e8ecfea8279",
+    measurementId: "G-C4L5VE611Z"
+  };
